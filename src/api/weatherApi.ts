8ab6f6b5d47513fdb1edfa151b2a00f2432c8f45
MatiIL <matibres@gmail.com>
2023-12-04 15:11:58 +0200

    changes API url to https

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -3,16 +3,16 @@ import { handleApiResponse } from "../utils";
 const apiKey = process.env.REACT_APP_ACCU_KEY;
 
 export const autocompleteSearch = async (query: string): Promise<any> => {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`;
+  const url = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`;
   return handleApiResponse(url);
 }
 
 export const dailyForecasts = async (id: string): Promise<any> => {
-  const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${apiKey}`;
+  const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${apiKey}`;
   return handleApiResponse(url);
 }
 
 export const getCurrentWeather = async (id: string): Promise<any> => {
-  const url = `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`;
+  const url = `https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`;
   return handleApiResponse(url);
 }
